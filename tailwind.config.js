e6d433545a7f14df705f95c4634752582ba05bac
Sergiusz Kriwobokow <writesd@hotmail.com>
2023-11-13 19:15:36 +0100

    created dashboard

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,37 @@
-/** @type {import('tailwindcss').Config} */
+
 module.exports = {
+  important: true,
   content: ["./**/*.{html,js}"],
+
+  // Active dark mode on class basis
+  darkMode: "class",
+  i18n: {
+    locales: ["en-US"],
+    defaultLocale: "en-US",
+  },
+  purge: {
+    content: ["./pages/**/*.tsx", "./components/**/*.tsx"],
+    // These options are passed through directly to PurgeCSS
+  },
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        check: "url('/icons/check.svg')",
+        landscape: "url('/images/landscape/2.jpg')",
+      }),
+    },
+  },
+  variants: {
+    extend: {
+      backgroundColor: ["checked"],
+      borderColor: ["checked"],
+      inset: ["checked"],
+      zIndex: ["hover", "active"],
+    },
   },
   plugins: [],
-}
+  future: {
+    purgeLayersByDefault: true,
+  },
+};
+
